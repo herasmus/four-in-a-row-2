2438ee60366c3336ea4db49809b04274b4b18291
Henrik Rasmussen <herasmus69@gmail.com>
2024-07-26 12:10:48 +0200

    Win detection for 4 rows nearly complete

--- src/main/java/dk/htr/games/minmax/four_in_row/board/winning/VerticalWinDetector.java
+++ src/main/java/dk/htr/games/minmax/four_in_row/board/winning/VerticalWinDetector.java
@@ -2,6 +2,7 @@ package dk.htr.games.minmax.four_in_row.board.winning;
 
 import dk.htr.games.minmax.four_in_row.config.GameDimensions;
 import dk.htr.games.minmax.four_in_row.exceptions.GameException;
+import org.apache.commons.lang3.NotImplementedException;
 
 import static dk.htr.games.minmax.four_in_row.bits.BitOperations.readByte;
 import static dk.htr.games.minmax.four_in_row.board.columns.four.ValidFourFowColumns.COLUMN_4R_OOOO;
@@ -10,24 +11,34 @@ import static dk.htr.games.minmax.four_in_row.board.columns.four.ValidFourFowCol
 public class VerticalWinDetector implements WinDetector {
     private final GameDimensions gameDimensions;
 
-    public VerticalWinDetector(GameDimensions gameDimensions) {
+    public VerticalWinDetector(GameDimensions gameDimensions) throws GameException {
         this.gameDimensions = gameDimensions;
     }
 
-    public WinResult hasWinner(long board, int move, boolean blueDisc) throws GameException {
-        boolean result = false;
-        if(gameDimensions.getLengthToWin() != 4) {
-            throw new GameException("Only support 4-in-row-to-win at the moment");
+    private WinResult getFourRowFourDiscWinner(long board, int move, boolean blueDisc) {
+        throw new NotImplementedException("FourRowThreeDiscWinner detection now implemented");
+    }
+
+    private WinResult getFourRowThreeDiscWinner(long board, int move, boolean blueDisc) {
+        throw new NotImplementedException("FourRowThreeDiscWinner detection now implemented");
+    }
+
+    private WinResult getFourRowWinner(long board, int move, boolean blueDisc) throws GameException {
+        if(gameDimensions.getLengthToWin() == 4) {
+            return getFourRowFourDiscWinner(board, move, blueDisc);
+        } else if(gameDimensions.getLengthToWin() == 3) {
+            return getFourRowThreeDiscWinner(board, move, blueDisc);
+        } else {
+            throw new GameException("Only support 3 and 4 discs in a row to win at the moment");
         }
-        if(gameDimensions.getNrOfRows() != 4) {
+    }
+
+    public WinResult getWinResult(long board, int move, boolean blueDisc) throws GameException {
+        boolean result = false;
+        if(gameDimensions.getNrOfRows() == 4) {
+            return getFourRowWinner(board, move, blueDisc);
+        } else {
             throw new GameException("Only support 4-rows at the moment");
         }
-        int column = readByte(board, move-1);
-        if(blueDisc) {
-            result = (column == COLUMN_4R_XXXX.getValue());
-        } else {
-            result = (column == COLUMN_4R_OOOO.getValue());
-        }
-        return new WinResult(result);
     }
 }
--- src/main/java/dk/htr/games/minmax/four_in_row/board/winning/WinDetector.java
+++ src/main/java/dk/htr/games/minmax/four_in_row/board/winning/WinDetector.java
@@ -3,5 +3,5 @@ package dk.htr.games.minmax.four_in_row.board.winning;
 import dk.htr.games.minmax.four_in_row.exceptions.GameException;
 
 public interface WinDetector {
-    WinResult hasWinner(long board, int move, boolean blueDisc) throws GameException;
+    WinResult getWinResult(long board, int move, boolean blueDisc) throws GameException;
 }
